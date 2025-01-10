27fb520e5a79f1350429f86ac7f09e55e4b6827e
Bastille-cmd <chetanchaudhary.18804@gmail.com>
2025-01-10 13:59:20 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 72.68%] - LeetPush

--- 75-Sort-Colors.java
+++ 75-Sort-Colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count1++;
+            }
+            if(nums[i]==1){
+                count2++;
+            }
+        }
+        for(int i=0;i<count1;i++){
+                nums[i]=0;
+        }
+        int temp2=count2;
+        for(int i=count1;i<count1+count2;i++){
+                nums[i]=1;
+        }
+        for(int i=count1+count2;i<nums.length;i++){
+            nums[i]=2;
+        }
     }
 }
