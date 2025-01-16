ca4c1f5a2838f5f670a57f115f585aac19bd75d5
Chetan Chaudhary <chetanchaudhary.18804@gmail.com>
2025-01-16 07:13:17 +0530

    [2486] [Time Beats: 41.74%] [Memory Beats: 92.15%] - LeetPush

--- 2486-Append-Characters-to-String-to-Make-Subsequence.java
+++ 2486-Append-Characters-to-String-to-Make-Subsequence.java
@@ -1,15 +1,13 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-        Stack<Character> stack=new Stack<>();
         int len=t.length();
-        for(int i=t.length()-1;i>=0;i--){
-            stack.push(t.charAt(i));
-        }
-        for(int i=0;i<=s.length()-1;i++){
-            if(stack.isEmpty()) return len;
-            if(s.charAt(i)==stack.peek()){
-                stack.pop();
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if(len>0){ 
+            if(s.charAt(i)==t.charAt(j)){
+                j++;
                 len--;
+            }
             }
         }
         return len;
