4191570b5eeb9b5b3deb5235e74d65ab8088ff0d
Chetan Chaudhary <chetanchaudhary.18804@gmail.com>
2025-01-25 09:19:48 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 26.59%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.java
+++ 104-Maximum-Depth-of-Binary-Tree.java
@@ -15,24 +15,11 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.offer(root);
-        int count=0;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            while(size>0){ 
-                TreeNode node = queue.poll();
-                size--;
-                if(node.left!=null){
-                    queue.offer(node.left);
-                }
-                if(node.right!=null){
-                    queue.offer(node.right);
-                }
-            }
-            count++;
+        if(root==null){
+            return 0;
         }
-        return count;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return 1+Math.max(left,right);
     }
 }
