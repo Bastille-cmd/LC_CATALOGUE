b11c6f22322d85e135ab3fc3c8375ae5658547ca
Chetan Chaudhary <chetanchaudhary.18804@gmail.com>
2025-02-01 00:10:47 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 59.86%] - LeetPush

--- 198-House-Robber.java
+++ 198-House-Robber.java
@@ -1,15 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        int n =nums.length;
-        if(nums == null || n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0],nums[1]);
-        int dp[]=new int[n];
-        dp[0]=nums[0];
-        dp[1]=Integer.max(nums[1],nums[0]);
-        for(int i=2;i<n;i++){
-            dp[i]=Integer.max(nums[i]+dp[i-2],dp[i-1]);
+        if(nums.length==1) return nums[0];
+        if(nums.length==1) return Math.max(nums[0],nums[1]);
+        int prev=nums[0];
+        int thiss=Math.max(nums[0],nums[1]);
+        int total=0;
+        for(int i=2;i<nums.length;i++){
+            total=Math.max((nums[i]+prev),thiss);
+            prev=thiss;
+            thiss=total;
         }
-        return dp[n-1];
+        return Math.max(prev,thiss);
     }
 }
